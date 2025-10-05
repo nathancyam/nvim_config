cff31341b03532ced5fe60c8546128427f48d691
Nathan Yam <nathancyam@gmail.com>
2025-10-06 10:24:35 +1100

    Add more advanced yank for neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -4,6 +4,52 @@ return {
     opts = {
       window = {
         position = "right",
+        mappings = {
+          ["l"] = "open",
+          ["h"] = "close_node",
+          ["<space>"] = "none",
+          ["Y"] = {
+            function(state)
+              -- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
+              -- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
+              local node = state.tree:get_node()
+              local filepath = node:get_id()
+              local filename = node.name
+              local modify = vim.fn.fnamemodify
+
+              local results = {
+                filepath,
+                modify(filepath, ":."),
+                modify(filepath, ":~"),
+                filename,
+                modify(filename, ":r"),
+                modify(filename, ":e"),
+              }
+
+              vim.ui.select({
+                "1. Absolute path: " .. results[1],
+                "2. Path relative to CWD: " .. results[2],
+                "3. Path relative to HOME: " .. results[3],
+                "4. Filename: " .. results[4],
+                "5. Filename without extension: " .. results[5],
+                "6. Extension of the filename: " .. results[6],
+              }, { prompt = "Choose to copy to clipboard:" }, function(choice)
+                local i = tonumber(choice:sub(1, 1))
+                local result = results[i]
+                vim.fn.setreg("+", result, "c")
+                vim.notify("Copied: " .. result)
+              end)
+            end,
+            desc = "Copy Path to Clipboard",
+          },
+          ["O"] = {
+            function(state)
+              require("lazy.util").open(state.tree:get_node().path, { system = true })
+            end,
+            desc = "Open with System Application",
+          },
+          ["P"] = { "toggle_preview", config = { use_float = false } },
+        },
       },
     },
   },
