9674e76a5377f34feef3ecf84c7f476712371e56
Nathan Yam <nathancyam@gmail.com>
2026-05-31 12:23:40 +1000

    Fixes

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,18 +3,17 @@ return {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
     opts = {
-      servers = {},
-      setup = {
-        expert = function(_, _)
-          vim.lsp.config("expert", {
-            cmd = { "expert", "--stdio" },
-            root_markers = { "mix.exs", ".git" },
-            filetypes = { "elixir", "eelixir", "heex" },
-          })
-
-          vim.lsp.enable("expert")
-        end,
+      servers = {
+        ["expert"] = {
+          cmd = { "expert", "--stdio" },
+          root_markers = { "mix.exs", ".git" },
+          filetypes = { "elixir", "eelixir", "heex" },
+        },
+        ["tailwindcss"] = {
+          filetypes_include = { "html", "heex", "eelixir" },
+        },
       },
+      setup = {},
     },
   },
 }
